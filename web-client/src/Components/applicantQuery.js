2e4b9692e7157280a8eb1ab0bd7748362dcb8c71
kawtherbouzeghaya <k_bouze@live.concordia.ca>
2023-03-18 03:40:35 -0400

    Notifications using firebase

--- web-client/src/Components/applicantQuery.js
+++ web-client/src/Components/applicantQuery.js
@@ -2,10 +2,12 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { useUserAuth } from "../firebase/UserAuthContext";
 import { useNavigate, Link } from "react-router-dom";
 import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
-import { firestore } from "../firebase/firebase";
+import { firestore, messaging } from "../firebase/firebase";
 import { Button } from "react-bootstrap";
 //import {CV_query} from './CV_query';
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
+//import 'firebase/compat/messaging';
+
 
 
 export default function ApplicantQuery(props) {
@@ -74,27 +76,87 @@ export default function ApplicantQuery(props) {
       });
         //CV_query(uid);
     }
+
+   function requestPermission() {
+    console.log('Requesting permission...');
+    Notification.requestPermission().then((permission) => {
+      if (permission == 'granted') {
+          console.log('Notification permission granted.');
+      }
+    })
+   }
+
+
+    async function handleCandidateSelected(uid) {
+      const playerDoc = await getDoc(doc(firestore, "Users", uid));
+      const playerId = playerDoc.data().playerId;
+    
+      // Create a new notification object for the selected candidate
+      const candidateNotification = {
+        notification: {
+          title: 'Congratulations!',
+          body: 'You have been selected for an interview.'
+        },
+        token: playerId
+      };
+    
+      // Send the notification to the selected candidate
+      try {
+        await messaging.send(candidateNotification);
+        console.log('Notification sent to candidate.');
+      } catch(error) {
+        console.log('Error sending notification to candidate:', error);
+      }
+    
+      // Create a new notification object for the employer
+      const employerNotification = {
+        notification: {
+          title: 'A candidate has been selected for the job.',
+          body: 'Please check your dashboard.'
+        },
+        token: playerDoc.data().employerId
+      };
+    
+      // Send the notification to the employer
+      try {
+        await messaging.send(employerNotification);
+        console.log('Notification sent to employer.');
+      } catch(error) {
+        console.log('Error sending notification to employer:', error);
+      }
+    }
+
+
+
   return (
-    <>
-    <div>applicantQuery</div>
-    <>
-        <h1>Applicants</h1>
-        <ul>
-            <table class="styled-table">
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>CV</th>
-                </tr>
-                </thead>
-                <tbody>
-            {applicants.map(app => <tr onClick={() => {}}//TBD whether it redirects to a new page
-            key={app.id}><td>{app.data.firstName} {app.data.lastName}</td><td>{app.data.email}</td><td><Button onClick={() => handleCVDownload(app.data.uid)}>view/download CV</Button></td></tr>)}
-            </tbody>
-            </table>
-        </ul>
-        </>
-    </>
-  )
+      <>
+      <div>applicantQuery</div>
+      <>
+          <h1>Applicants</h1>
+          <ul>
+              <table class="styled-table">
+                  <thead>
+                  <tr>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>CV</th>
+                      <th>Candidate Selection </th>
+                  </tr>
+                  </thead>
+                  <tbody>
+              {applicants.map(app => <tr onClick={() => {}}//TBD whether it redirects to a new page
+              key={app.id}><td>{app.data.firstName} {app.data.lastName}</td><td>{app.data.email}</td><td><Button onClick={() => handleCVDownload(app.data.uid)}>view/download CV</Button>
+              </td>
+  
+              <td> <Button onClick={() => handleCandidateSelected(app.data.uid)}>Select Candidate</Button>
+  
+              </td>
+  
+              </tr>)}
+              </tbody>
+              </table>
+          </ul>
+          </>
+      </>
+    )
 }
--- web-client/src/firebase/firebase.js
+++ web-client/src/firebase/firebase.js
@@ -4,6 +4,7 @@ import { getAuth } from "firebase/auth";//import { getAnalytics } from "firebase
 import { getFirestore } from "@firebase/firestore";
 import firebase from "./firebase";
 import {getStorage} from "firebase/storage"
+import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -17,11 +18,27 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize firebase cloud messaging. 
+export const messaging = getMessaging(app);
+getToken(messaging, {vapidKey:"AAAAJSh2LeI:APA91bHyDjH3FuQMVk1-Oh21La4ZkmfHMukbiHA_3gAot-WNEkIDi2EXAG-Wq2SMocVPtiHDH9CFFC_PyVe8GsX2xl4TfEgqoqabGnBplis1IkS6TVYAbQnUyNjiKaHhMAMkAGRA820p"}).then((currentToken) =>{
+  if (currentToken) {
+
+  } else {
+    console.log('No registration token available');
+  }
+}).catch((err) => {
+  console.log('An error occured while tertieving token', err);
+});
+ 
 //const analytics = getAnalytics(app);
 export const firestore = getFirestore(app);
 
 export const auth = getAuth();
 export default app;
 export const storage = getStorage(app);
+
+
+
 
 //------------------------------------------------
